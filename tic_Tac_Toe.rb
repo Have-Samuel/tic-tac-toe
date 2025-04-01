235684ed8e8f98e072fb142a99efdfea1232f207
samuel <samiehave9@gmail.com>
2025-04-01 10:38:06 +0300

    Commenting

---
+++ tic_Tac_Toe.rb
@@ -0,0 +1,9 @@
+# We will have four classes:
+# 1. Game: This class will handle the game logic.
+# 2. Player: This class will represent a player.
+# 3. HumanPlayer: This class will represent a human player.
+# 4. ComputerPlayer: This class will represent a computer player.
+
+# A one-dimensional array will be used to represent the board, handcodes all possible straight lines rather than storing the board as a 2D matrix, and indenifying straight lines dynamically.
+# The game will be played in a 3x3 grid, and the players will take turns to place their marks (X or O) on the grid.
+# The game will check for a winner after each move, and the game will end when there is a winner or the board is full (a draw).
