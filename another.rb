7233215757e2dd112c74ddb4540a709e6cbc76a7
samuel <samiehave9@gmail.com>
2025-04-02 16:52:27 +0300

    Using the Rubocop

--- another.rb
+++ another.rb
@@ -1,6 +1,6 @@
 class Game
-  @@turn_count = 1
-  @@winner = ''
+  @turn_count = 1
+  @winner = ''
 
   def initialize
     puts 'Welcome to Tic Tac Toe!'
@@ -31,7 +31,7 @@ class Game
   def player_choice(player, symbol)
     puts "#{player} Please enter your coordinatesseperated by a space (row column):"
     input = gets.chomp
-    input_array = input.split(' ')
+    input_array = input.split
     coord_one = input_array[0].to_i
     coord_two = input_array[1].to_i
 
@@ -43,96 +43,96 @@ class Game
       input_array[0].to_i
       input_array[1].to_i
     end
-    
+
     add_to_board(coord_one, coord_two, symbol)
   end
 
   def add_to_board(coord_one, coord_two, symbol)
     @board[coord_one][coord_two] = symbol
-    @@turn_count += 1
+    @turn_count += 1
     # display_board(@board)
     # check_winner(symbol)
   end
 
   # Check 3 across
-    def three_across
-      @board.each do |i|
-         if i.all? { |j| j == 'X' }
-           @@winner = @player_one_name
-           @@turn_count = 10
-         elsif i.all? { |j| j == 'O' }
-           @@winner = @player_two_name
-           @@turn_count = 10
-         end
+  def three_across
+    @board.each do |i|
+      if i.all? { |j| j == 'X' }
+        @winner = @player_one_name
+        @turn_count = 10
+      elsif i.all? { |j| j == 'O' }
+        @winner = @player_two_name
+        @turn_count = 10
+      end
+    end
+  end
+
+  # Check 3 down
+  def three_down
+    flat = @board.flatten
+    # Flatten the board and check if any of the columns have 3 in a row
+    flat.each_with_index do |v, i|
+      if v == 'X' && flat[i + 3] == 'X' && flat[i + 6] == 'X'
+        @winner = @player_one_name
+        @turn_count = 10
+      elsif v == 'O' && flat[i + 3] == 'O' && flat[i + 6] == 'O'
+        @winner = @player_two_name
+        @turn_count = 10
+      end
+    end
+  end
+
+  # Check 3 diagonal
+  def three_diagonal
+    center_val = @board[1][1]
+    if %w[X O].include?(center_val)
+      if @board[0][0] == center_val && @board[2][2] == center_val
+        @winner = @player_one_name if center_val == 'X'
+        @winner = @player_two_name if center_val == 'O'
+        @turn_count = 10
+      elsif @board[0][2] == center_val && @board[2][0] == center_val
+        @winner = @player_one_name if center_val == 'X'
+        @winner = @player_two_name if center_val == 'O'
+        @turn_count = 10
       end
     end
 
-    # Check 3 down
-    def three_down
-      flat = @board.flatten
-      # Flatten the board and check if any of the columns have 3 in a row
-      flat.each_with_index do |v, i|
-        if v == 'X' && flat[i + 3] == 'X' && flat[i + 6] == 'X'
-          @@winner = @player_one_name
-          @@turn_count = 10
-        elsif v == 'O' && flat[i + 3] == 'O' && flat[i + 6] == 'O'
-          @@winner = @player_two_name
-          @@turn_count = 10
-        end
+    def declare_result(symbol)
+      case symbol
+      when '0'
+        puts "#{@player_one_name} wins the game!"
+      when '1'
+        puts "#{@player_two_name} wins the game!"
+      else
+        puts "It's a draw!"
       end
     end
 
-    # Check 3 diagonal
-    def three_diagonal
-      center_val = @board[1][1]
-      if center_val == 'X' || center_val == 'O'
-        if @board[0][0] == center_val && @board[2][2] == center_val
-          @@winner = @player_one_name if center_val == 'X'
-          @@winner = @player_two_name if center_val == 'O'
-          @@turn_count = 10
-        elsif @board[0][2] == center_val && @board[2][0] == center_val
-          @@winner = @player_one_name if center_val == 'X'
-          @@winner = @player_two_name if center_val == 'O'
-          @@turn_count = 10
-        end
+    def play_game
+      puts "\r\n"
+      puts 'Here is your empty battlefield:'
+      display_board(@board)
+
+      until @turn_count == 10
+        player_turn(@turn_count)
+        three_across
+        three_down
+        three_diagonal
+        display_board(@board)
       end
 
-      def declare_result(symbol)
-        case symbol
-        when '0'
-            puts "#{@player_one_name} wins the game!"
-        when '1'
-            puts "#{@player_two_name} wins the game!"
-        else
-            puts "It's a draw!"
-        end
-      end
-
-      def play_game
-        puts "\r\n"
-        puts "Here is your empty battlefield:"
-        display_board(@@board)
-
-        until @@turn_count == 10
-          player_turn(@@turn_count)
-            three_across
-            three_down
-            three_diagonal
-            display_board(@@board)
-        end
-
-        declare_result(@@winner)
-      end
-
+      declare_result(@winner)
+    end
+  end
 end
 
 # Instructions
-puts "Welcome to Tic Tac Toe!, The rules are as expected, but choosing placement requires coordinates."
-puts "Each turn, Enter two numbers with a space, per the grid layout below:"
+puts 'Welcome to Tic Tac Toe!, The rules are as expected, but choosing placement requires coordinates.'
+puts 'Each turn, Enter two numbers with a space, per the grid layout below:'
 puts "\r\n"
-puts "0 0 | 0 1 | 0 2"
-puts "1 0 | 1 1 | 1 2"
-puts "2 0 | 2 1 | 2 2"
+puts '0 0 | 0 1 | 0 2'
+puts '1 0 | 1 1 | 1 2'
+puts '2 0 | 2 1 | 2 2'
 puts "\r\n"
 
 # Instantiate the game and execute play game
